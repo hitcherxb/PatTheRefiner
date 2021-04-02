7a358ded93db5bcb838efee1a8c659c170839438
Jonny van Essen <jve3094@gmail.com>
2021-04-02 12:22:51 -0400

    added img links

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ console.log("test")
 document.querySelector(".imgLink").onclick = function (e) {
     window.location.href = "About\ Me/aboutMe.html"
 }
+
+
+let projectImg = document.querySelectorAll('.projectlink')
+
+for (img of projectImg) {
+    img.onclick = function (e) {
+        window.location.href = "Case\ Study/casestudy.html"
+    }
+}
+
