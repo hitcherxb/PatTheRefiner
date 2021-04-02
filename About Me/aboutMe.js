eb28a947c32f70fb42d90cfec260eff18503521f
Cody Thigpen <ckthigpen1@gmail.com>
2021-04-02 11:23:50 -0400

    Easter Egg

--- About Me/aboutMe.js
+++ About Me/aboutMe.js
@@ -1,3 +1,16 @@
 function burgerTransition(e) {
     e.classList.toggle("change");
 }
+
+document.addEventListener("keypress", function onPress(event) {
+    if (event.key === "z" && event.ctrlKey) {
+        // document.body.style.backgroundColor = 'green';
+        document.body.style.backgroundImage = "url('/images/space.jpeg')";
+        document.querySelector('#face').src = '/images/alien.jpeg'
+        document.querySelector('.nameText h1').innerHTML = 'HTM aLiens!'
+        document.getElementById("name").style.color = "green";
+        document.querySelector("p").style.color = "green";
+        document.querySelector('.nameText p').innerHTML = '⟟ ⏃⋔ ⏃ ☊⍀⟒⏃⏁⟟⎐⟒ ⎍⌖/⎍⟟ ⎅⟒⌇⟟☌⋏⟒⍀ ⍙⟟⏁⊑ ⍜⎐⟒⍀ ⏁⟒⋏ ⊬⟒⏃⍀⌇ ⍜⎎ ⟒⌖⌿⟒⍀⟟⟒⋏☊⟒. ⟟ ⏃⋔ ⍙⟒⌰⌰-⎐⟒⍀⌇⟒⎅ ⟟⋏ ⎎⟟☌⋔⏃, ⏃⎅⍜⏚⟒⌖⎅, ⌿⊑⍜⏁⍜⌇⊑⍜⌿, ⌇☍⟒⏁☊⊑, ⌇⌰⏃☊☍, ⏚⏃⌰⌇⏃⋔⟟⍾, ⊑⏁⋔⌰, ⏃⋏⎅ ☊⌇⌇. ⋔⊬ ⌇⌿⟒☊⟟⏃⌰⏁⟟⟒⌇ ⏃⍀⟒⟟⌰⌰⎍⌇⏁⍀⏃⏁⟟⍜⋏ ⏃⋏⎅ ☌⍀⏃⌿⊑⟟☊ ⎅⟒⌇⟟☌⋏, ⏃⋏⎅ ⟟ ⎎⍜☊⎍⌇ ⍜⋏ ⟒⋏⊑⏃⋏☊⟟⋏☌ ⏁⊑⟒ ⎐⟟⌇⎍⏃⌰ ⟒⌖⌿⟒⍀⟟⟒⋏☊⟒, ⏚⏃⌰⏃⋏☊⟟⋏☌ ⎎⍜⍀⋔ ⏃⋏⎅ ⎎⎍⋏☊⏁⟟⍜⋏, ⏃⋏⎅ ⟟⋏☊⍀⟒⏃⌇⟟⋏☌⏃☊☊⟒⌇⌇⟟⏚⟟⌰⟟⏁⊬ ⎎⍜⍀ ⎅⟒⌇☍⏁⍜⌿ ⏃⋏⎅ ⋔⍜⏚⟟⌰⟒ ⎍⌇⟒⍀⌇. ⎎⟒⟒⌰ ⎎⍀⟒⟒ ⏁⍜ ⏚⍀⍜⍙⌇⟒ ⋔⊬ ⌿⏃⌇⏁ ⌿⍀⍜⟊⟒☊⏁⌇ ⏃⋏⎅ ☌⟒⏁ ⟟⋏ ⏁⍜⎍☊⊑ ⍙⟟⏁⊑ ⋔⟒ ⍜⋏ ⌇⍜☊⟟⏃⌰ ⋔⟒⎅⟟⏃!'
+
+    }
+});
